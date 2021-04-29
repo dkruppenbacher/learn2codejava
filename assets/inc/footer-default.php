daf3fc4a872af30d4ff395eb6e3413791f9563cd
iNatie <62487654+iNatie@users.noreply.github.com>
2021-04-29 17:03:22 -0400

    Dark mode added
    Added a dark mode that respects user's OS dark mode setting, and has a toggle button on the footer. It writes a cookie of the last state so when the user comes back it remembers if they had light or dark mode.

--- assets/inc/footer-default.php
+++ assets/inc/footer-default.php
@@ -1,5 +1,21 @@
 <footer>       
+    <button class="btn-toggle">Toggle Dark Mode</button>
     <p class="footer"> @2021 GearMonke Inc.&emsp;&emsp;<a href="mailto:info@learn2codejava.com">Contact: info@learn2codejava.com</a>&emsp;&emsp;<a href="#">Donate</a></p>
 </footer>
+	<script>
+  const btn = document.querySelector(".btn-toggle");
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+  
+  btn.addEventListener("click", function() {
+    if (prefersDarkScheme.matches) {
+      document.body.classList.toggle("light-mode");
+      var theme = document.body.classList.contains("light-mode") ? "light" : "dark";
+    } else {
+      document.body.classList.toggle("dark-mode");
+      var theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
+    }
+    document.cookie = "theme=" + theme+"; path=/";
+  });
+</script>
  </body>
  </html>
