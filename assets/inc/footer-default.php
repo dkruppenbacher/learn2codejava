d2dfa804371ce24713348aafb452a2a801115c99
iNatie <62487654+iNatie@users.noreply.github.com>
2021-04-29 00:28:34 -0400

    Setup for smart sideNav

--- assets/inc/footer-default.php
+++ assets/inc/footer-default.php
@@ -1,3 +1,3 @@
 <footer>       
-    <p class="footer"> @2021 GearMonke Inc.&emsp;&emsp;<a href="mailto:info@learn2codejava.com">Contact: info@learn2codejava.com</a></p>
+    <p class="footer"> @2021 GearMonke Inc.&emsp;&emsp;<a href="mailto:info@learn2codejava.com">Contact: info@learn2codejava.com</a>&emsp;&emsp;<a href="#">Donate</a></p>
 </footer>
