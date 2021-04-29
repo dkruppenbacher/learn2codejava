c1d15d43f7f9dca6d8c41b943e4c1c2c4f501045
iNatie <62487654+iNatie@users.noreply.github.com>
2021-04-28 23:58:42 -0400

    fixed nav

--- assets/inc/nav-home-default.php
+++ assets/inc/nav-home-default.php
@@ -1,7 +1,7 @@
     <nav>
     <ul class="navigation">
-      <li><a class="active" href="<?php echo $path; ?>index.html">Home</a></li>
-      <li><a href="<?php echo $path; ?>content.html">Content</a></li>
+      <li><a class="active" href="<?php echo $path; ?>index.php">Home</a></li>
+      <li><a href="<?php echo $path; ?>content/content.php">Content</a></li>
       <li><a href="<?php echo $path; ?>#">About Us</a></li>
       <li><a href="<?php echo $path; ?>#">Donate</a></li>
     </ul>
