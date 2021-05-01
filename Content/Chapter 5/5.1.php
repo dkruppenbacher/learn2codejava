b01d6541006e5cfd52bdf22b3fc938b63a78734a
emmathomas36 <eet2341@rit.edu>
2021-05-01 12:31:46 -0400

    Lessons 5.1 and 5.2

---
+++ Content/Chapter 5/5.1.php
@@ -0,0 +1,53 @@
+<?php
+  $page = "Lesson 5.1";
+  $pagetype = "content";
+  $path = "../../";
+  $mydir1 = dirname(__FILE__);
+  $myd = substr($mydir1, 40);
+  include($path.'assets/inc/head-default.php');
+?>
+<body>
+<?php
+  include($path.'assets/inc/nav-content-default.php');
+  include($path.'assets/inc/sideNav-default.php');
+?>
+  <div class='pagecontent'>
+    <p>Arrays are simple structures used to create a collection of items of the same type. Each item in an array can be accessed by it's position number, known as it's index. Lets look at the instantiation of an array:</p>
+    <!--Use <code> to define a code block. Use <br> to make line breaks, and a &emsp;&emsp; to define a tab-->
+    <code>
+      TYPE[] NAME = new TYPE[number elements]; <br>
+    </code><br>
+      <p>Let's break it down:</p>
+      <ul>
+        <li> 'TYPE' is the data type you want to be in the array. Every element in an array has to be of the same type.</li>
+        <li>'NAME' is the name that you give the array in order to access the data in the array</li>
+        <li>The number of elements is the number of elements you wish to place in the array. This number cannot change once it is set.</li>
+        <li>The numbering of indices ALWAYS starts at 0 and the last index is the number of elements - 1. For example, if you initialize an array of 10 ints, the indices will go from 0 to 9.</li>
+    </ul>
+    <p>There is another way to initialize an array, which is done by</p>
+      <code>
+        TYPE[] NAME = {Value1, Value2, Value3 ...}; <br>  
+      </code><br>
+      <p>This instantiation is used when you want to store all of the data for the array right away.</p>
+      <p>Let's take a look at the ways to access and modify the array. Here is how you would access a specific index.</p>
+      <code>
+        NAME[INDEX]; <br>
+      </code><br>
+      <p>The name is the name you gave the array, and the index is the index of the data you want to access.</p>
+      <p>To add a value to this index, you would take the statement above and assign it a value:</p>
+      <code>
+        NAME[INDEX] = newValue; <br>
+      </code> <br>
+      <p>Finally, to get the length of the array, you would use</p>
+      <code>
+        NAME.length; <br>
+      </code><br>
+      
+      <p>Although arrays can be helpful, there are still many limitations to them. The size is fixed, which means the array cannot grow or shrink after it is instantiated. You also cannot easily add or delete values from the middle of the array; only at one end. For this reason, ArrayLists are commonly used. Continue to 5.2 to learn about them!</p>
+  </div>
+</div>
+<!-- footer-normal.php start -->
+<?php
+  include($path.'assets/inc/footer-default.php');
+?>
+<!-- footer-normal.php end-->
---
+++ Content/Chapter 5/5.2.php
@@ -0,0 +1,41 @@
+<?php
+  $page = "Lesson 5.2";
+  $pagetype = "content";
+  $path = "../../";
+  $mydir1 = dirname(__FILE__);
+  $myd = substr($mydir1, 40);
+  include($path.'assets/inc/head-default.php');
+?>
+<body>
+<?php
+  include($path.'assets/inc/nav-content-default.php');
+  include($path.'assets/inc/sideNav-default.php');
+?>
+  <div class='pagecontent'>
+    <p>ArrayList is a class in java, which means we must <code>import java.util.*;</code> in order to access it. To declare an ArrayList, we use the following form:</p>
+    <code>
+        ArrayList&lt;String&gt; list = new ArrayList&lt;String&gt;(); <br>
+    </code>
+    <!--Use <code> to define a code block. Use <br> to make line breaks, and a &emsp;&emsp; to define a tab-->
+    <p>Initially this list has no elements, but it will grow as you add more elements. In this specific ArrayList, there will only be String elements. All elements must be of the instantiated type, just like an array. They are also numbered starting at 0.</p>
+    <p>To add to an ArrayList, you use he add method, <code>list.add("Stawberries");</code>. This causes the list to grow by one slot, adding 'Strawberries' to the end of the list.</p>
+    <p>To get the current size of the list, you use <code>list.size();</code></p>
+    <p>To get an item at a specified position, n, use <code>String s = list.get(n);</code> This saves the String at position n to the new String variable s.</p>
+    <p>You can also add a new value at any position, even if something exists already. This is done by <code>list.add(5, "Grapes");</code> This puts 'Grapes' at position 5, which moves all existing positions 5, 6, 7... to 6, 7, 8, and so on. It pushes apart the list to open up position 5 for 'Grapes'.</p>
+    <p>You can also change a value at a specific location, using <code>list.set(n, "new value");</code></p>
+    <p>To remove an item, use <code>list.remove(n);</code> This removes the item at this position, which then moves all te items after that one back down one spot. For example, if we remove, if we remove 3, then 4, 5, 6 would now be in spots 3, 4, 5.</p>
+    <p>ArrayLists can be made of any object or class, not just String. Because of this, Java has created wrapper classes of the primitive types. For example, if you need a list of ints, you would use the wrapper class 'Integer' and instantiate it as <code>ArrayList&lt;Integer&gt; list = new ArrayList&lt;Integer&gt;();</code> Here are the wrapper classes:</p>
+    <ul>
+        <li>Integer = int</li>
+        <li>Double = double</li>
+        <li>Character = char</li>
+        <li>Boolean = boolean</li>
+    </ul>
+    <p>Both arrays and ArrayLists can be used with for loops. Lesson 5.3 will take you through examples of these structures using loops.</p>
+  </div>
+</div>
+<!-- footer-normal.php start -->
+<?php
+  include($path.'assets/inc/footer-default.php');
+?>
+<!-- footer-normal.php end-->
