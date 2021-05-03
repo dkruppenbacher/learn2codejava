fbdc11f2e8873f92202bf8bd80ac24c8fdf52b54
emmathomas36 <eet2341@rit.edu>
2021-05-02 20:01:15 -0400

    defaults to 1.1 when content is clicked

--- assets/inc/nav-home-default.php
+++ assets/inc/nav-home-default.php
@@ -1,7 +1,7 @@
     <nav>
     <ul class="navigation">
       <li><a class="active" href="<?php echo $path; ?>index.php">Home</a></li>
-      <li><a href='<?php echo $path; ?>Content/'>Content</a></li>
+      <li><a href='<?php echo $path; ?>Content/Chapter 1/1.1.php'>Content</a></li>
       <li><a href="<?php echo $path; ?>#">About Us</a></li>
       <li><a href="<?php echo $path; ?>#">Donate</a></li>
     </ul>
