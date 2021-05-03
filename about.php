a64e24e6f687b60c59830d264b9d97e438b7cf6a
IsaiahRivera17 <79813749+IsaiahRivera17@users.noreply.github.com>
2021-05-03 16:58:04 -0400

    Add files via upload
    Still needs pics and descriptions for each person.

---
+++ about.php
@@ -0,0 +1,89 @@
+<!-- head-normal.php start -->
+<?php
+  $page = "Home";
+  $pagetype = "home";
+  $path = './';
+  $mydir1 = dirname(__FILE__);
+  $myd = substr($mydir1, 40);
+  include($path.'assets/inc/head-default.php');
+?>
+<!-- head-normal.php end-->
+
+<!-- nav-home-normal.php start -->
+<?php
+  include($path.'assets/inc/nav-home-default.php');
+?>
+<!-- nav-home-normal.php end -->
+<div class="head">
+    <h2 >About GearMonke</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<br>
+
+
+<h2>Meet The Team</h2>
+  </div>  
+    <div class="center">
+    <div class="Contain">
+        <div class="card">
+            <div class="imgBx">
+                <img src="<?php echo $path; ?>assets/img/Isaiah_profilePic.jpg" alt="Isaiah Rivera">
+            </div>
+            <div class="content">
+                <h3>Interactive Designer - Isaiah R.</h3>
+                <p>Isaiah Rivera is a first year Human-Centered Computing student and was the Interactive Designer for this site he  developed the navigation from the wireframes that were created by the information architect and graphic designer. His goal was to make the navigation simple and easy to follow along so that the user does not get lost while also making it interactive.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="imgBx">
+                <img src="<?php echo $path; ?>assets/img/Dalton_profilePic.png" alt="Dalton K.">
+            </div>
+            <div class="content">
+                <h3>Team Leader - Dalton K.</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus egestas sed sed risus pretium. Pulvinar neque laoreet suspendisse interdum consectetur. Sit amet venenatis urna cursus eget. Sed faucibus turpis in eu mi bibendum neque egestas congue. Sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Tellus id interdum velit laoreet id donec. Eget lorem dolor sed viverra ipsum nunc aliquet. Magna fermentum iaculis eu non. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="imgBx">
+                <img src="<?php echo $path; ?>assets/img/Isaiah_profilePic.jpg" alt="Isaiah Rivera">
+            </div>
+            <div class="content">
+                <h3>Information Architect - Nathaniel P.</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus egestas sed sed risus pretium. Pulvinar neque laoreet suspendisse interdum consectetur. Sit amet venenatis urna cursus eget. Sed faucibus turpis in eu mi bibendum neque egestas congue. Sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Tellus id interdum velit laoreet id donec. Eget lorem dolor sed viverra ipsum nunc aliquet. Magna fermentum iaculis eu non. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="imgBx">
+                <img src="<?php echo $path; ?>assets/img/Isaiah_profilePic.jpg" alt="Isaiah Rivera">
+            </div>
+            <div class="content">
+                <h3>Graphic Design - Emma T.</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus egestas sed sed risus pretium. Pulvinar neque laoreet suspendisse interdum consectetur. Sit amet venenatis urna cursus eget. Sed faucibus turpis in eu mi bibendum neque egestas congue. Sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Tellus id interdum velit laoreet id donec. Eget lorem dolor sed viverra ipsum nunc aliquet. Magna fermentum iaculis eu non. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="imgBx">
+                <img src="<?php echo $path; ?>assets/img/Isaiah_profilePic.jpg" alt="Isaiah Rivera">
+            </div>
+            <div class="content">
+                <h3>Content Creator - Trevor Y.</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus egestas sed sed risus pretium. Pulvinar neque laoreet suspendisse interdum consectetur. Sit amet venenatis urna cursus eget. Sed faucibus turpis in eu mi bibendum neque egestas congue. Sed ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Tellus id interdum velit laoreet id donec. Eget lorem dolor sed viverra ipsum nunc aliquet. Magna fermentum iaculis eu non. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet.</p>
+            </div>
+        </div>
+        
+        
+        
+    </div>
+</div>       
+            
+      
+
+
+
+
+
+<!-- footer-normal.php start -->
+<?php
+  include($path.'assets/inc/footer-default.php');
+?>
+<!-- footer-normal.php end -->
