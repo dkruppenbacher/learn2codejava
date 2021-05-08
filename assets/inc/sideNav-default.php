dfba6f592c6ff688230a6a7216b78b5f4d446827
iNatie <62487654+iNatie@users.noreply.github.com>
2021-05-08 17:26:57 -0400

    bugfix 2

--- assets/inc/sideNav-default.php
+++ assets/inc/sideNav-default.php
@@ -3,61 +3,61 @@
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="<?php echo $path; ?>content/Chapter 1/1.1.php">1.1</a>
-    <a href="<?php echo $path; ?>content/Chapter 1/Chapter 1 quiz.php">Quiz</a>
+    <a href="<?php echo $path; ?>Content/Chapter 1/1.1.php">1.1</a>
+    <a href="<?php echo $path; ?>Content/Chapter 1/Chapter 1 quiz.php">Quiz</a>
   </div>
     <button class="dropdown-btn">Chapter 2
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="<?php echo $path; ?>content/Chapter 2/2.1.php">2.1</a>
-    <a href="<?php echo $path; ?>content/Chapter 2/2.2.php">2.2</a>
-    <a href="<?php echo $path; ?>content/Chapter 2/Chapter 2 quiz.php">Quiz</a>
+    <a href="<?php echo $path; ?>Content/Chapter 2/2.1.php">2.1</a>
+    <a href="<?php echo $path; ?>Content/Chapter 2/2.2.php">2.2</a>
+    <a href="<?php echo $path; ?>Content/Chapter 2/Chapter 2 quiz.php">Quiz</a>
   </div>
     <button class="dropdown-btn">Chapter 3
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="<?php echo $path; ?>content/Chapter 3/3.1.php">3.1</a>
-    <a href="<?php echo $path; ?>content/Chapter 3/3.2.php">3.2</a>
-    <a href="<?php echo $path; ?>content/Chapter 3/3.3.php">3.3</a>
-    <a href="<?php echo $path; ?>content/Chapter 3/Chapter 3 quiz.php">Quiz</a>
+    <a href="<?php echo $path; ?>Content/Chapter 3/3.1.php">3.1</a>
+    <a href="<?php echo $path; ?>Content/Chapter 3/3.2.php">3.2</a>
+    <a href="<?php echo $path; ?>Content/Chapter 3/3.3.php">3.3</a>
+    <a href="<?php echo $path; ?>Content/Chapter 3/Chapter 3 quiz.php">Quiz</a>
   </div>
     <button class="dropdown-btn">Chapter 4
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="<?php echo $path; ?>content/Chapter 4/4.1.php">4.1</a>
-    <a href="<?php echo $path; ?>content/Chapter 4/4.2.php">4.2</a>
-    <a href="<?php echo $path; ?>content/Chapter 4/Chapter 4 quiz.php">Quiz</a>
+    <a href="<?php echo $path; ?>Content/Chapter 4/4.1.php">4.1</a>
+    <a href="<?php echo $path; ?>Content/Chapter 4/4.2.php">4.2</a>
+    <a href="<?php echo $path; ?>Content/Chapter 4/Chapter 4 quiz.php">Quiz</a>
   </div>
   <button class="dropdown-btn">Chapter 5
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="<?php echo $path; ?>content/Chapter 5/5.1.php">5.1</a>
-    <a href="<?php echo $path; ?>content/Chapter 5/5.2.php">5.2</a>
-    <a href="<?php echo $path; ?>content/Chapter 5/Chapter 5 quiz.php">Quiz</a>
+    <a href="<?php echo $path; ?>Content/Chapter 5/5.1.php">5.1</a>
+    <a href="<?php echo $path; ?>Content/Chapter 5/5.2.php">5.2</a>
+    <a href="<?php echo $path; ?>Content/Chapter 5/Chapter 5 quiz.php">Quiz</a>
   </div>
   <button class="dropdown-btn">Chapter 6
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="<?php echo $path; ?>content/Chapter 6/6.1.php">6.1</a>
-    <a href="<?php echo $path; ?>content/Chapter 6/6.2.php">6.2</a>
-    <a href="<?php echo $path; ?>content/Chapter 6/6.3.php">6.3</a>
-    <a href="<?php echo $path; ?>content/Chapter 6/Chapter 6 quiz.php">Quiz</a>
+    <a href="<?php echo $path; ?>Content/Chapter 6/6.1.php">6.1</a>
+    <a href="<?php echo $path; ?>Content/Chapter 6/6.2.php">6.2</a>
+    <a href="<?php echo $path; ?>Content/Chapter 6/6.3.php">6.3</a>
+    <a href="<?php echo $path; ?>Content/Chapter 6/Chapter 6 quiz.php">Quiz</a>
   </div>
   <button class="dropdown-btn">Chapter 7
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <a href="<?php echo $path; ?>content/Chapter 7/7.1.php">7.1</a>
-    <a href="<?php echo $path; ?>content/Chapter 7/7.2.php">7.2</a>
-    <a href="<?php echo $path; ?>content/Chapter 7/7.3.php">7.3</a>
-    <a href="<?php echo $path; ?>content/Chapter 7/Chapter 7 quiz.php">Quiz</a>
+    <a href="<?php echo $path; ?>Content/Chapter 7/7.1.php">7.1</a>
+    <a href="<?php echo $path; ?>Content/Chapter 7/7.2.php">7.2</a>
+    <a href="<?php echo $path; ?>Content/Chapter 7/7.3.php">7.3</a>
+    <a href="<?php echo $path; ?>Content/Chapter 7/Chapter 7 quiz.php">Quiz</a>
   </div>
-  <button class='sidenav-btn' onclick="window.location='<?php echo $path; ?>content/Project 1.php'">Project 1</button>
+  <button class='sidenav-btn' onclick="window.location='<?php echo $path; ?>Content/Project 1.php'">Project 1</button>
 </div>
 <script> 
 var dropdown = document.getElementsByClassName("dropdown-btn");
