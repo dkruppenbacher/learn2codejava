41fd77d776f22c7cd44ba8c3ba078050f66eb471
IsaiahRivera17 <79813749+IsaiahRivera17@users.noreply.github.com>
2021-05-08 18:35:00 -0400

    Update nav-content-default.php
    added comment to nav

--- assets/inc/nav-content-default.php
+++ assets/inc/nav-content-default.php
@@ -4,6 +4,6 @@
       <li><a <?php echo (isset($pagetype) && $pagetype=='content') ? ' class="active" ' :'' ?> class='active' href='<?php echo $path; ?>Content/Chapter 1/1.1.php'>Content</a></li>
       <img src="<?php echo $path; ?>assets/img/gearmonke_logo.svg" alt="GearMonkeLogo" class="logo">
       <li><a <?php echo (isset($pagetype) && $pagetype=='aboutus') ? ' class="active" ' :'' ?> href="<?php echo $path; ?>about.php">About Us</a></li>
-      <li><a <?php echo (isset($pagetype) && $pagetype=='donate') ? ' class="active" ' :'' ?> href="<?php echo $path; ?>#">Donate</a></li>
+      <li><a <?php echo (isset($pagetype) && $pagetype=='donate') ? ' class="active" ' :'' ?> href="<?php echo $path; ?>comment.php">Comment</a></li>
     </ul>
 </nav>
