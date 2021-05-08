22052067365e7e84021bdbb8eabd31f1d53526a4
IsaiahRivera17 <79813749+IsaiahRivera17@users.noreply.github.com>
2021-05-08 18:38:10 -0400

    Update nav-home-default.php
    added to nav

--- assets/inc/nav-home-default.php
+++ assets/inc/nav-home-default.php
@@ -3,6 +3,6 @@
       <li><a class="active" href="<?php echo $path; ?>index.php">Home</a></li>
       <li><a href='<?php echo $path; ?>Content/Chapter 1/1.1.php'>Content</a></li>
       <li><a href="<?php echo $path; ?>about.php">About Us</a></li>
-      <li><a href="<?php echo $path; ?>#">Donate</a></li>
+      <li><a href="<?php echo $path; ?>comments.php">Comments</a></li>
     </ul>
     </nav>
