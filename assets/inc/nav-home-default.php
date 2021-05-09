9647d3a2fdb5093f8829997ba6890de7d4faffa2
iNatie <62487654+iNatie@users.noreply.github.com>
2021-05-08 21:26:12 -0400

    bugfix

--- assets/inc/nav-home-default.php
+++ assets/inc/nav-home-default.php
@@ -2,7 +2,6 @@
     <ul class="navigation">
       <li><a <?php echo (isset($pagetype) && $pagetype=='home') ? ' class="active" ' :'' ?> href="<?php echo $path; ?>index.php">Home</a></li>
       <li><a <?php echo (isset($pagetype) && $pagetype=='content') ? ' class="active" ' :'' ?> href='<?php echo $path; ?>Content/Chapter 1/1.1.php'>Content</a></li>
-      <img src="<?php echo $path; ?>assets/img/gearmonke_logo.svg" alt="GearMonkeLogo" class="logo">
       <li><a <?php echo (isset($pagetype) && $pagetype=='aboutus') ? ' class="active" ' :'' ?> href="<?php echo $path; ?>about.php">About Us</a></li>
       <li><a <?php echo (isset($pagetype) && $pagetype=='comment') ? ' class="active" ' :'' ?> href="<?php echo $path; ?>comments.php">Comments</a></li>
     </ul>
